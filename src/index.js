dafcc569019b8d85b84d4bfdd12fac409892e791
Justyn Silva <justynhagen@gmail.com>
2023-03-07 12:55:12 -0600

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import './less/index.less'
 
 // Your code goes here!
+const signUpBottom = document.querySelector('#btn')
+const navLinks = document.querySelector("#nav-link")
+const title = document.querySelector("h1")
+const introImg = document.querySelector("")
+introImg.classList.add('Content-Img')
+
+
+// * [ ] Here are some event types you could try to use:
+//   * [ ] `mouseover`
+//   * [ ] `keydown`
+//   * [ ] `wheel`
+//   * [ ] `load`
+//   * [ ] `focus`
+//   * [ ] `resize`
+//   * [ ] `scroll`
+//   * [ ] `select`
+//   * [ ] `dblclick`
+//   * [ ] `drag / drop`
