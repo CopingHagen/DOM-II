f86c0c9f992d6780dff52999ce160532dafcfb2a
Justyn Silva <justynhagen@gmail.com>
2023-03-07 13:08:01 -0600

    added 3 events

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import './less/index.less'
 
 // Your code goes here!
-const signUpBottom = document.querySelector('#btn')
-const navLinks = document.querySelector("#nav-link")
-const title = document.querySelector("h1")
-const introImg = document.querySelector("")
-introImg.classList.add('Content-Img')
+
+
+// 1- load
+window.onload = function (evt) {
+    console.log(`event ${evt.type} fired! Ready to go!`)
+    const heading = document.querySelector('h1')
+    heading.textContent = 'Ready to go!'
+    console.log(heading)
+
+    // 2- copy
+    window.addEventListener('copy', () => {
+        navigator.clipboard.readText()
+            .then(text => {
+                heading.textContent += text
+            })
+    })
+
+    // 3- click
+    document.body.addEventListener('click', evt => {
+        evt.target.classList.toggle('mirror')
+    })
+}
+
+
 
 
 // * [ ] Here are some event types you could try to use:
